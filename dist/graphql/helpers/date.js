97a602941578f5d54fee73462bb675d240ecb1c5
arthurhovhannisyan31 <arthurhovhannisyan31@gmail.com>
2020-11-21 20:15:48 +0400

    0

--- dist/graphql/helpers/date.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// global imports
-// local imports
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.dateToISOString = void 0;
-exports.dateToISOString = (date) => new Date(date).toISOString();
-//# sourceMappingURL=date.js.map
