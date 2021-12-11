35c611f673359115df2ffd6aca3d859dc7ee21a3
Artur Oganesian <arthurhovhannisyan31@gmail.com>
2021-12-11 05:23:15 +0400

    fix server config

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,3 @@
-import http from 'http'
-import https from 'https'
-
 import MongoStore from 'connect-mongo'
 import express from 'express'
 import { graphqlHTTP } from 'express-graphql'
@@ -8,50 +5,32 @@ import session from 'express-session'
 import mongoose from 'mongoose'
 
 import {
-  __PROD__,
   CONNECT_CONFIG,
   getSessionMdlOptions,
-  httpsOptions,
   mongoOptions,
 } from './constants/config'
 import { resolvers } from './graphql/resolvers'
 import { schema } from './graphql/schema'
-import {
-  addSecurityHeaders,
-  customCorsCheck,
-  customFormatError,
-  redirectToHttps,
-  redirectToRoot,
-} from './utils/helpers'
+import { customCorsCheck, customFormatError } from './utils/helpers'
 
 const main = async (): Promise<void> => {
-  const rootApp = express()
-  const httpsApp = express()
-  const httpApp = express()
-
+  const app = express()
   const sessionStore = MongoStore.create({
     mongoUrl: CONNECT_CONFIG.SESSION_DB_CONNECTION_STRING,
     mongoOptions,
     touchAfter: 24 * 3600,
   })
 
-  httpsApp.set('port', process.env.HTTPS_PORT || 443)
-  httpsApp.use(redirectToRoot)
-
-  httpApp.set('port', process.env.HTTP_PORT || 80)
-  httpApp.use(redirectToHttps)
-
-  rootApp.set('port', process.env.ROOT_PORT || 4002)
-  rootApp.set('trust proxy', true)
-  rootApp.use(customCorsCheck)
-  rootApp.use(express.json())
-  rootApp.use(
+  app.use(customCorsCheck)
+  app.use(express.json())
+  app.use(
     express.urlencoded({
       extended: true,
     })
   )
-  rootApp.use(session(getSessionMdlOptions(sessionStore)))
-  rootApp.use(
+  app.set('trust proxy', true)
+  app.use(session(getSessionMdlOptions(sessionStore)))
+  app.use(
     '/graphql',
     graphqlHTTP((req, res) => ({
       schema,
@@ -65,37 +44,13 @@ const main = async (): Promise<void> => {
       },
     }))
   )
-  rootApp.use(addSecurityHeaders)
   try {
     await mongoose.connect(CONNECT_CONFIG.DB_CONNECTION_STRING, mongoOptions)
-    http.createServer(httpApp).listen(httpApp.get('port'), () => {
-      if (!__PROD__) {
-        console.log(
-          `HTTP server started at http://localhost:${httpApp.get('port')}`
-        )
-      }
-    })
-    https
-      .createServer(httpsOptions, httpsApp)
-      .listen(httpsApp.get('port'), () => {
-        if (!__PROD__) {
-          console.log(
-            `HTTPS server started at https://localhost:${httpsApp.get('port')}`
-          )
-        }
-      })
-    rootApp.listen(rootApp.get('port'))
-
-    if (!__PROD__) {
-      console.log(
-        `GraphQL server started at http://localhost:${rootApp.get('port')}`
-      )
-      console.log(
-        `Please see graphql environment at http://localhost:${rootApp.get(
-          'port'
-        )}/graphql`
-      )
-    }
+    app.listen(CONNECT_CONFIG.PORT)
+    console.log(`Server started at http://localhost:${CONNECT_CONFIG.PORT}`)
+    console.log(
+      `Please see graphql environment at http://localhost:${CONNECT_CONFIG.PORT}/graphql`
+    )
   } catch (err) {
     console.log(err)
   }
